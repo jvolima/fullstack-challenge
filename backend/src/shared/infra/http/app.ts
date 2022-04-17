602a960e75dcaa10f1f4e5feb5603f7233557a4e
João Vitor Lima <jvolima2004@gmail.com>
2022-04-17 16:01:03 -0300

    configurando docker-compose e adicionando cors na aplicação

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,10 +1,26 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import { routes } from "./routes";
+import { IAppError } from "core/domain/errors/IAppError";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use(routes);
+
+app.use(
+  (
+    error: IAppError,
+    _request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    return response.status(error.statusCode || 500).json({
+      message: error.message
+    });
+  }
+);
 
 export { app }
